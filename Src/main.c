056304bc9f0b381a522b36e5ea0b5baa10ab3458
Łukasz Nidecki <qyonek+github@gmail.com>
2017-04-01 18:29:38 +0200

    Working

--- Src/main.c
+++ Src/main.c
@@ -79,11 +79,11 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-//  MX_DMA_Init();
-//  MX_USART1_UART_Init();
-//  MX_USART2_UART_Init();
-//  MX_TIM2_Init();
-//  MX_TIM3_Init();
+  MX_DMA_Init();
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+  MX_TIM2_Init();
+  MX_TIM3_Init();
 
   /* USER CODE BEGIN 2 */
 
